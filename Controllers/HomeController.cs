70eb0976a39c45de74e42f22736479363390815c
Student <example@example.com>
2020-03-09 11:57:06 -0700

    create project environment. add Models and Index View

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MadLibs.Models;
+
+namespace MadLibs.Controllers
+{
+  public class HomeController : Controller
+  {
+    
+  }
+}
---
+++ Models/MadLibsVariable.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace MadLibs.Solution.Models
+{
+  public class MadLibsVariable
+  {
+    public string Adj1 { get; set; }
+    public string Adj2 { get; set; }
+    public string Noun1 { get; set; }
+    public string Noun2 { get; set; }
+    public string PluralNoun1 { get; set; }
+    public string Game1 { get; set; }
+    public string PluralNoun2 { get; set; }
+    public string VerbIng1 { get; set; }
+    public string VerbIng2 { get; set; }
+    public string PluralNoun3 { get; set; }
+    public string VerbIng3 { get; set; }
+    public string Noun3 { get; set; }
+    public string Plant1 { get; set; }
+    public string Body1 { get; set; }
+    public string Place1 { get; set; }
+    public string VerbIng4 { get; set; }
+    public string Adj3 { get; set; }
+    public string Number { get; set; }
+    public string PluralNoun4 { get; set; }
+
+  }
+}
